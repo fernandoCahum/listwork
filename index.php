2b356b1e3d8e6f0d3d70e7e5793d29a2b608f7ac
Fernando Cahum <fcbacab@hotmail.com>
2018-08-20 22:42:21 -0500

    index de la página

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+print('
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="./css/style.css">
+  <title>To Do List </title>
+</head>
+<body>
+  <h1>To Do List con PHP y MySQL</h1>
+  <div class="container">
+    <table>
+      <tr>
+        <th>id</th>
+        <th>Tarea</th>
+        <th> </th>
+      </tr>
+');
+
+
+print('
+    </table>
+  </div>
+  <footer>
+    <p>Fernando Cahum Bacab</p>
+    <p>Cancun Q. Roo 2018</p>
+  </footer>
+</body>
+</html>
+');
